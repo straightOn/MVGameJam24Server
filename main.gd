e1ee5f78a09342c4d1555a394774e13b479bbfc5
robert <robert@s7on.de>
2024-12-07 01:47:57 +0100

    updates

--- main.gd
+++ main.gd
@@ -3,17 +3,31 @@ extends Node2D
 @onready var connection_handler: ConnectionHandler = %ConnectionHandler
 @onready var player_resorce: Resource = preload("res://shared/player.tscn")
 
+const ObjectTypeResource = preload("res://shared/object_type.gd")
+
 var connected_players: Dictionary = {}
 
 func _ready():
-	connection_handler.create_server()
 	connection_handler.player_connect_event.connect(_player_connect)
 	connection_handler.player_disconnect_event.connect(_player_disconnect)
-	connection_handler.player_move_x_event.connect(_player_move_x_event)
-	connection_handler.player_move_y_event.connect(_player_move_y_event)
 	connection_handler.player_move_event.connect(_player_move_event)
+	connection_handler.player_join_game_event.connect(_player_join_game)
+	connection_handler.create_server()
 	
 func _player_connect(peer_id: int):
+	connection_handler.send_game_state(peer_id)
+
+func _player_disconnect(peer_id: int):
+	if (connected_players.has(peer_id)):
+		remove_child(connected_players.get(peer_id))
+		connected_players.erase(peer_id)
+		connection_handler.object_removed(peer_id)
+
+func _player_move_event(peer_id: int, input: Vector2):
+	if (connected_players.has(peer_id)):
+		(connected_players.get(peer_id) as Player).move_action(input)
+
+func _player_join_game(peer_id: int):
 	if (!connected_players.has(peer_id)):
 		var initial_position: Vector2 = Vector2(50,50)
 		var player: Player = player_resorce.instantiate() as Player
@@ -25,22 +39,4 @@ func _player_connect(peer_id: int):
 		connected_player.position_changed_event.connect(connection_handler.object_position_update)
 		for key in connected_players:
 			var current_player = connected_players[key]
-			connection_handler.object_created(key, "", current_player.position)
-
-func _player_disconnect(peer_id: int):
-	if (connected_players.has(peer_id)):
-		remove_child(connected_players.get(peer_id))
-		connected_players.erase(peer_id)
-		connection_handler.object_removed(peer_id)
-	
-func _player_move_x_event(peer_id: int, input: float):
-	if (connected_players.has(peer_id)):
-		(connected_players.get(peer_id) as Player).move_x_action(input)
-	
-func _player_move_y_event(peer_id: int, input: float):
-	if (connected_players.has(peer_id)):
-		(connected_players.get(peer_id) as Player).move_y_action(input)
-	
-func _player_move_event(peer_id: int, input: Vector2):
-	if (connected_players.has(peer_id)):
-		(connected_players.get(peer_id) as Player).move_action(input)
+			connection_handler.object_created(key, ObjectTypeResource.ObjectType.Player, current_player.position)
--- services/connection_handler.gd
+++ services/connection_handler.gd
@@ -7,6 +7,7 @@ signal player_disconnect_event(peer_id: int)
 signal player_move_x_event(peer_id: int, input: float)
 signal player_move_y_event(peer_id: int, input: float)
 signal player_move_event(peer_id: int, input: Vector2)
+signal player_join_game_event(peer_id: int)
 
 var active_connections: Array[int] = []
 
@@ -28,7 +29,7 @@ func _on_peer_disconnected(peer_id):
 	active_connections.erase(peer_id)
 	player_disconnect_event.emit(peer_id)
 	
-func object_created(id: int, type: String, initial_position: Vector2):
+func object_created(id: int, type: ObjectTypeResource.ObjectType, initial_position: Vector2):
 	print_debug("Object created.")
 	for connection in active_connections:
 		rpc_id(connection, "receive_object_created", id, type, initial_position)
@@ -42,6 +43,15 @@ func object_position_update(id: int, position: Vector2):
 	print_debug("Updating Object position.")
 	for connection in active_connections:
 		rpc_id(connection, "receive_object_position_update", id, position)
+
+func send_game_state(peer_id: int):
+	rpc_id(peer_id, "receive_game_state", active_connections.size(), ConnectionConstants.MAX_CONNECTIONS)
+
+@rpc("any_peer")
+func join_game():
+	super.join_game()
+	var sender_id = multiplayer.get_remote_sender_id()
+	self.join_game_event.emit(sender_id)
 
 @rpc("any_peer")
 func move_action(direction: Vector2):
